a3b10ffe4d0a0fb5ab7557b09befb6334e3beb1a
Travis Cline <travis.cline@gmail.com>
2009-10-18 16:29:30 -0500

    added packaging bits

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='mutter',
+    version='0.0.1',
+    description='Smart little muttering command runner',
+    author='Travis cline',
+    author_email='travis.cline@gmail.com',
+    url='http://github.com/traviscline/mutter',
+    packages=['mutter'],
+    scripts = ['bin/mutter'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Utilities'],
+)
